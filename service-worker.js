6cbd5c346e322451ec61679d38ee336b0619ef4d
Beno Pinantau Rohman <rohman13@users.noreply.github.com>
2020-07-05 04:02:06 +0700

    directory error

--- service-worker.js
+++ service-worker.js
@@ -6,22 +6,22 @@ else
     console.log(`Workbox gagal dimuat`);
 
 workbox.precaching.precacheAndRoute([
-    { url: '/index.html', revision: '1' },
-    { url: '/nav.html', revision: '1' },
-    { url: '/bundle.js', revision: '1' },
-    { url: '/styles/preloader-only.css', revision: '1' },
-    { url: '/fonts/MaterialIcons-Regular.eot', revision: '1' },
-    { url: '/fonts/MaterialIcons-Regular.ttf', revision: '1' },
-    { url: '/fonts/MaterialIcons-Regular.woff', revision: '1' },
-    { url: '/fonts/MaterialIcons-Regular.woff2', revision: '1' },
-    { url: '/img/no-img.png', revision: '1' },
-    { url: '/img/icon-72.png', revision: '1' },
-    { url: '/img/icon-128.png', revision: '1' },
-    { url: '/img/icon-192.png', revision: '1' },
-    { url: '/img/icon-384.png', revision: '1' },
-    { url: '/img/icon-512.png', revision: '1' },
-    { url: '/img/icon-apple-192.png', revision: '1' },
-    { url: '/manifest.json', revision: '1' },
+    { url: 'index.html', revision: '1' },
+    { url: 'nav.html', revision: '1' },
+    { url: 'bundle.js', revision: '1' },
+    { url: 'styles/preloader-only.css', revision: '1' },
+    { url: 'fonts/MaterialIcons-Regular.eot', revision: '1' },
+    { url: 'fonts/MaterialIcons-Regular.ttf', revision: '1' },
+    { url: 'fonts/MaterialIcons-Regular.woff', revision: '1' },
+    { url: 'fonts/MaterialIcons-Regular.woff2', revision: '1' },
+    { url: 'img/no-img.png', revision: '1' },
+    { url: 'img/icon-72.png', revision: '1' },
+    { url: 'img/icon-128.png', revision: '1' },
+    { url: 'img/icon-192.png', revision: '1' },
+    { url: 'img/icon-384.png', revision: '1' },
+    { url: 'img/icon-512.png', revision: '1' },
+    { url: 'img/icon-apple-192.png', revision: '1' },
+    { url: 'manifest.json', revision: '1' },
 ]);
 
 workbox.routing.registerRoute(
